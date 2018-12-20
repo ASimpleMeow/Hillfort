ff31338ed7a0782bb0f537f1f0623534bc13f28d
Oleksandr Kononov <sashakon13@gmail.com>
2018-12-20 15:33:25 +0000

    Add favorite variable to HillfortModel

--- app/src/main/java/org/wit/hillfort/views/hillfortlist/HillfortAdapter.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfortlist/HillfortAdapter.kt
@@ -3,6 +3,7 @@ package org.wit.hillfort.views.hillfortlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.card_hillfort.view.*
 import org.wit.hillfort.R
@@ -32,7 +33,24 @@ class HillfortAdapter constructor(private var hillforts: List<HillfortModel>,
       itemView.hillfortTitle.text = hillfort.title
       itemView.description.text = hillfort.description
       if (hillfort.images.isNotEmpty()) Glide.with(itemView.context).load(hillfort.images.first()).into(itemView.imageIcon)
+      itemView.visited_icon.setOnClickListener {
+        hillfort.visited = !hillfort.visited
+        showIcons(hillfort, itemView.fav_icon, itemView.visited_icon)
+      }
+      itemView.fav_icon.setOnClickListener {
+        hillfort.favorite = !hillfort.favorite
+        showIcons(hillfort, itemView.fav_icon, itemView.visited_icon)
+      }
+      showIcons(hillfort, itemView.fav_icon, itemView.visited_icon)
       itemView.setOnClickListener { listener.onHillfortClick(hillfort) }
+    }
+
+    private fun showIcons(hillfort: HillfortModel, favIcon: ImageView, visitedIcon: ImageView){
+      if (hillfort.favorite) favIcon.setImageResource(R.drawable.ic_favorite_black_24dp)
+      else favIcon.setImageResource(R.drawable.ic_favorite_border_black_24dp)
+
+      if (hillfort.visited) visitedIcon.setImageResource(R.drawable.ic_visibility_black_24dp)
+      else visitedIcon.setImageResource(R.drawable.ic_visibility_off_black_24dp)
     }
   }
 }
